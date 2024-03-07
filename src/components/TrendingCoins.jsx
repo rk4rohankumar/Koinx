9f2269d2907efd184da1827121b2492c6279cccb
Saikrishna <lingams2001@gmail.com>
2024-03-07 19:56:45 +0530

    footer is now responsive

--- src/components/TrendingCoins.jsx
+++ src/components/TrendingCoins.jsx
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 const TrendingCoins = () => {
     const [trendingData, setTrendingData] = useState(null);
+    const [slides, setSlides] = useState(1);
 
     const fetchTrendingData = async () => {
         try {
@@ -24,10 +25,42 @@ const TrendingCoins = () => {
         fetchTrendingData();
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            const windowWidth = window.innerWidth;
+            let newSlides;
+
+            if (windowWidth <= 480) {
+                newSlides = 1;
+            } else if (windowWidth < 845) {
+                newSlides = 2;
+            } else if (windowWidth < 992) {
+                newSlides = 3;
+            } else if (windowWidth < 1200) {
+                newSlides = 4;
+            } else {
+                newSlides = 5;
+            }
+
+            setSlides(newSlides);
+        };
+
+        // Initial call to setSlides based on the initial window width
+        handleResize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const settings = {
         dots: false,
         infinite: true,
-        slidesToShow: 5,
+        slidesToShow: slides,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
