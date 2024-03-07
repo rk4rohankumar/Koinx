9bec82b63d66cd42af49e55b45249ebda798127c
Saikrishna <lingams2001@gmail.com>
2024-03-07 17:02:45 +0530

    changed tokenomics

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -9,7 +9,7 @@ const Tokenomics = () => {
             <div className='m-5'>
                 <h1>Initial Distribution</h1>
                 <img src="" alt="" />
-
+        Doughnut Chart
             </div>
             <div className='m-5 pb-5 '>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum explicabo repellendus magni odit repellat reprehenderit facilis blanditiis architecto id itaque, perspiciatis sapiente animi adipisci iusto sed deserunt vel modi natus obcaecati corporis, assumenda inventore molestias ducimus vero. Ea exercitationem nemo perferendis eveniet recusandae velit officia earum architecto doloribus praesentium? Quasi assumenda aspernatur accusamus inventore perferendis, perspiciatis consectetur atque sit doloribus fuga magni placeat pariatur, natus deserunt minus!</p>
